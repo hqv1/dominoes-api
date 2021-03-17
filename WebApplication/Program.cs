59ddb04c16b71d77b05f93e96420010338b34ef7
Viet Hoang <v.hoang@outlook.com>
2021-03-16 18:51:10 -0700

    Added Dockerfile. Rename namespace and application file.

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace WebApplication
+namespace Hqv.Dominoes.WebApplication
 {
     public static class Program
     {
