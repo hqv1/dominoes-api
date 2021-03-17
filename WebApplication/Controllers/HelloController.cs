492e09a82e02b7ebb423660ff7af449473de180a
Viet H <v.hoang@outlook.com>
2021-03-16 19:08:19 -0700

    Added Dockerfile. (#9)
    Rename namespace and application file.

--- WebApplication/Controllers/HelloController.cs
+++ WebApplication/Controllers/HelloController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplication.Controllers
+namespace Hqv.Dominoes.WebApplication.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -1,18 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace WebApplication
+namespace Hqv.Dominoes.WebApplication
 {
     public class Startup
     {
@@ -40,7 +33,7 @@ namespace WebApplication
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dominoes WebApplication v1"));
             }
 
             app.UseHttpsRedirection();
